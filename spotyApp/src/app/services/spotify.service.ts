5830e902133577d1046595bac5dcedf8b192abe9
panapunk <panapunk@gmail.com>
2019-11-17 12:44:22 +0100

    subida del proyecto

---
+++ spotyApp/src/app/services/spotify.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  _newToken:string = 'BQDJ8R8rgNu1ft4T4tYPnStFbN7HuF46YpeYGGeFqOKe50xp0SPlPWCz4NY8iMCBvQ9oe3vba7Rnk6QDAMQ';
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify Service Listo');
+  }
+
+  getQuery( query: string ) {
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this._newToken
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe( map( data => data['albums'].items ));
+
+  }
+
+  getArtistas( termino: string ) {
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( data => data['artists'].items));
+
+  }
+
+  getArtista( id: string ) {
+
+    return this.getQuery(`artists/${ id }`);
+                // .pipe( map( data => data['artists'].items));
+
+  }
+
+  getTopTracks( id: string ) {
+
+    return this.getQuery(`artists/${ id }/top-tracks?country=es`)
+                .pipe( map( data => data['tracks']));
+
+  }
+
+}
